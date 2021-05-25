c2d50e7039d4217a2e330dc06ad55ee0fac299de
YoungHan-Jo <81964439+YoungHan-Jo@users.noreply.github.com>
2021-05-25 15:01:25 +0900

    0525

--- src/java0520/Ex3.java
+++ src/java0520/Ex3.java
@@ -39,26 +39,80 @@ public class Ex3 {
 		System.out.println("제일 큰 수 : " + max);
 
 		int min = numArr[0];
-		
-		for (int i = 1; i<numArr.length;i++) {
-			if (min >numArr[i]) {
+
+		for (int i = 1; i < numArr.length; i++) {
+			if (min > numArr[i]) {
 				min = numArr[i];
 			}
 		}
 		System.out.println("제일 작은 수 : " + min);
-		
+
 		int a = 10;
 		int b = 20;
-		
+
 		int tmp = a;
 		a = b;
 		b = tmp;
-		
-		System.out.println("a = " + a +"\n"+ "b = " + b);
-		
-		
-		
-		
-	} // main
 
+		System.out.println("a = " + a + "\n" + "b = " + b);
+
+		System.out.println("=====================================");
+
+		for (int i = 0; i < numArr.length; i++) {
+			System.out.print(numArr[i] + " ");
+		}
+		System.out.println();
+
+		// 0번째 인덱스를 임시변수로 만들어 놓고 [0]에 제일 큰 숫자를 대입함
+		// 79 88 91 33 100 55 95 81 75 64 // [0] [1] 더 크면 교환
+		// 88 79 91 33 100 55 95 81 75 64 // [0] [2] 더 크면 교환
+
+		// 내림차순 정렬
+		// numArr[0] 최대값
+
+		for (int i = 1; i < numArr.length; i++) {
+			if (numArr[0] < numArr[i]) { // [0]과 나머지 인덱스랑 비교해서 교환하기
+				tmp = numArr[0];
+				numArr[0] = numArr[i];
+				numArr[i] = tmp;
+			}
+
+		}
+
+		for (int i = 0; i < numArr.length; i++) {
+			System.out.print(numArr[i] + " ");
+		}
+		System.out.println();
+
+		// [1] 부터 최대값구하기
+		for (int i = 2; i < numArr.length; i++) {
+			if (numArr[1] < numArr[i]) { // [0]과 나머지 인덱스랑 비교해서 교환하기
+				tmp = numArr[1];
+				numArr[1] = numArr[i];
+				numArr[i] = tmp;
+			}
+
+		}
+
+		for (int i = 0; i < numArr.length; i++) {
+			System.out.print(numArr[i] + " ");
+		}
+		System.out.println();
+
+		// 최종 내림차순
+		for (int i = 0; i < numArr.length; i++) {
+			for (int j = i + 1; j < numArr.length; j++) {
+				if (numArr[i] < numArr[j]) { // [0]과 나머지 인덱스랑 비교해서 교환하기
+					tmp = numArr[i];
+					numArr[i] = numArr[j];
+					numArr[j] = tmp;
+				}
+			}
+		}
+
+		for (int i = 0; i < numArr.length; i++) {
+			System.out.print(numArr[i] + " ");
+		}
+		System.out.println();
+	} // main
 }
