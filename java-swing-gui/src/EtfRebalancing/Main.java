13d7c7416a8c36aa7326f63b1fa458501a3082e9
JYH <verin4494@gmail.com>
2021-07-04 21:07:17 +0900

    0704

--- java-swing-gui/src/EtfRebalancing/Main.java
+++ java-swing-gui/src/EtfRebalancing/Main.java
@@ -8,6 +8,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import java.awt.event.ActionListener;
 
 public class Main extends JFrame {
 
@@ -31,37 +32,63 @@ public class Main extends JFrame {
 
 	private void init() {
 		this.lbHoldStock = new JLabel("보유 주식");
+		lbHoldStock.setBounds(69, 24, 74, 15);
 		this.lbEtfPrice = new JLabel("1주 가격");
+		lbEtfPrice.setBounds(303, 24, 70, 15);
 		this.lbHoldDollar = new JLabel("보유 달러");
+		lbHoldDollar.setBounds(69, 58, 74, 15);
 		this.lbHoldWon = new JLabel("보유 원");
+		lbHoldWon.setBounds(313, 58, 60, 15);
 		this.lbAddWon = new JLabel("추가 투입(원)");
+		lbAddWon.setBounds(69, 95, 88, 15);
 		this.lbExRateOneDollar = new JLabel("1$ 당 환율");
+		lbExRateOneDollar.setBounds(299, 95, 74, 15);
 		this.lbSettingRate = new JLabel("비율 설정");
+		lbSettingRate.setBounds(69, 138, 74, 15);
 		this.lbResultStock = new JLabel("리밸런싱 후 보유 주식");
+		lbResultStock.setBounds(90, 238, 120, 15);
 		this.lbResultCash = new JLabel("리밸런싱 후 보유 현금");
+		lbResultCash.setBounds(93, 274, 120, 15);
 		this.lbTotal = new JLabel("총 합계");
+		lbTotal.setBounds(169, 316, 40, 15);
 
 		this.tfHoldStock = new JTextField(8);
+		tfHoldStock.setBounds(169, 21, 94, 21);
 		this.tfEtfPrice = new JTextField(5);
+		tfEtfPrice.setBounds(396, 21, 94, 21);
 		this.tfHoldDollar = new JTextField(8);
+		tfHoldDollar.setBounds(169, 55, 94, 21);
 		this.tfHoldWon = new JTextField(8);
+		tfHoldWon.setBounds(396, 52, 94, 21);
 		this.tfAddWon = new JTextField(10);
+		tfAddWon.setBounds(169, 92, 94, 21);
 		this.tfExRateOneDollar = new JTextField(10);
+		tfExRateOneDollar.setBounds(396, 92, 94, 21);
 		this.tfSettingRate = new JTextField(10);
+		tfSettingRate.setBounds(169, 135, 94, 21);
 		this.tfResultStock = new JTextField(8);
+		tfResultStock.setBounds(239, 235, 94, 21);
 		this.tfResultStockPer = new JTextField(5);
+		tfResultStockPer.setBounds(345, 235, 61, 21);
 		this.tfResultCash = new JTextField(8);
+		tfResultCash.setBounds(239, 271, 94, 21);
 		this.tfResultCashPer = new JTextField(5);
+		tfResultCashPer.setBounds(345, 271, 61, 21);
 		this.tfTotal = new JTextField(10);
+		tfTotal.setBounds(239, 313, 116, 21);
 
 		this.board = new JTextArea(1, 50);
+		board.setBounds(118, 189, 354, 24);
+		board.setText("매수(매도)해야할 수량 : ");
+		board.setEnabled(false);
 
 		this.btn = new JButton("계산하기");
+		btn.setBounds(391, 134, 99, 23);
 	}// end of init
 
 	private void setComponents() {
 		Container c = getContentPane();
-		c.setLayout(new FlowLayout());
+		getContentPane().setLayout(null);
 		c.add(lbHoldStock);
 		c.add(tfHoldStock);
 		c.add(lbEtfPrice);
@@ -109,13 +136,13 @@ public class Main extends JFrame {
 
 			if (targetStock >= holdStock) {
 				toBuyCnt = (int) Math.round((targetStock - holdStock) / etfPrice);
-				this.board.setText(toBuyCnt + "주 매수");
+				this.board.setText("매수(매도)해야할 수량 : " + toBuyCnt + "주 매수");
 				resultStock = holdStock + toBuyCnt * etfPrice;
 				resultCash = total - resultStock;
 
 			} else {
 				toSellCnt = (int) Math.round((holdStock - targetStock) / etfPrice);
-				this.board.setText(toSellCnt + "주 매도");
+				this.board.setText("매수(매도)해야할 수량 : " + toSellCnt + "주 매도");
 				resultStock = holdStock - toSellCnt * etfPrice;
 				resultCash = total - resultStock;
 			}
@@ -134,7 +161,7 @@ public class Main extends JFrame {
 	private void showFrame() {
 		setTitle("Etf 리밸런싱 계산기");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(600, 200);
+		setSize(600, 416);
 		setLocationByPlatform(true);
 		setVisible(true);
 	}// end of showFrame
