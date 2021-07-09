39319afaf18f2e7dd5fad692ebff472a6889a2c7
JYH <verin4494@gmail.com>
2021-07-10 01:04:21 +0900

    0710

--- PosProgram/src/com/example/app/view/MainView.java
+++ PosProgram/src/com/example/app/view/MainView.java
@@ -2,21 +2,71 @@ package com.example.app.view;
 
 import java.awt.CardLayout;
 import java.awt.Container;
+import java.awt.Dimension;
+import java.util.Calendar;
 
 import javax.swing.JPanel;
 
 import com.example.app.StoreManager;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import java.awt.FlowLayout;
+import javax.swing.BoxLayout;
+import javax.swing.JTextArea;
+import javax.swing.UIManager;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
-public class MainView implements Viewable {
+public class MainView implements Viewable, Runnable {
 
 	public static final String VIEW_NAME = "main";
-	public static final int WIDTH = 700;
-	public static final int HEIGHT = 600;
+	public static final int WIDTH = 1200;
+	public static final int HEIGHT = 700;
+
+	private Thread thread;
 
 	private CardLayout cardLayout;
 	private Container container;
 	private StoreManager frame;
 	private JPanel panelMain;
+	private JPanel panelHeader;
+	private JPanel panelFooter;
+	private JPanel panelBody;
+	private JLabel lblMainView;
+	private JLabel lblCurrentTime;
+	private JPanel panelLeft;
+	private JPanel panelRight;
+	private JPanel panelTables;
+	private JPanel panelOrder;
+	private JPanel panelTableNum1;
+	private JPanel panelTableHeader1;
+	private JPanel panel_2;
+	private JLabel lblNewLabel_1;
+	private JLabel lblNewLabel_2;
+	private JLabel lblTableNum1;
+	private JLabel lblTableTime1;
+	private JTextArea textArea;
+	private JLabel lblNewLabel;
+	private JLabel lblAdults;
+	private JLabel lblKids;
+	private JTextField tfAdults;
+	private JTextField tfKids;
+	private JButton btnAdmission;
+	private JLabel lblSoju;
+	private JLabel lblBeer;
+	private JTextField tfSoju;
+	private JTextField tfBeer;
+	private JLabel lblBeverage;
+	private JLabel lblSoup;
+	private JTextField tfBeverage;
+	private JTextField tfSoup;
+	private JLabel lblDessert;
+	private JTextField tfDessert;
+	private JButton btnRenewOrder;
+	private JButton btnAdmissionCancel;
+	private JButton btnPayment;
 
 	public MainView(CardLayout cardLayout, Container container, StoreManager frame) {
 		this.cardLayout = cardLayout;
@@ -33,14 +83,201 @@ public class MainView implements Viewable {
 		return panelMain;
 	}
 
+	@Override
+	public void run() {
+		while (true) {
+			Calendar cal = Calendar.getInstance();
+			String now = cal.get(Calendar.YEAR) + "년 "
+					+ ((cal.get(Calendar.MONTH) + 1) < 10 ? "0" + (cal.get(Calendar.MONTH) + 1)
+							: (cal.get(Calendar.MONTH) + 1))
+					+ "월 " + (cal.get(Calendar.DATE) < 10 ? "0" + cal.get(Calendar.DATE) : cal.get(Calendar.DATE))
+					+ "일 "
+					+ (cal.get(Calendar.HOUR_OF_DAY) < 10 ? "0" + cal.get(Calendar.HOUR_OF_DAY)
+							: cal.get(Calendar.HOUR_OF_DAY))
+					+ "시" + (cal.get(Calendar.MINUTE) < 10 ? "0" + cal.get(Calendar.MINUTE) : cal.get(Calendar.MINUTE))
+					+ "분" + (cal.get(Calendar.SECOND) < 10 ? "0" + cal.get(Calendar.SECOND) : cal.get(Calendar.SECOND))
+					+ "초 ";
+			lblCurrentTime.setText(now);
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
 	private void init() {
 		panelMain = new JPanel();
+		panelMain.setSize(WIDTH, HEIGHT);
+		panelMain.setLayout(new BorderLayout(0, 0));
+
+		panelHeader = new JPanel();
+		panelMain.add(panelHeader, BorderLayout.NORTH);
+		panelHeader.setLayout(new BorderLayout(0, 0));
+
+		lblMainView = new JLabel("POS");
+		lblMainView.setFont(new Font("굴림", Font.BOLD, 25));
+		panelHeader.add(lblMainView, BorderLayout.CENTER);
+
+		lblCurrentTime = new JLabel("");
+
+		panelFooter = new JPanel();
+		panelMain.add(panelFooter, BorderLayout.SOUTH);
+
+		panelBody = new JPanel();
+		panelMain.add(panelBody, BorderLayout.CENTER);
+		panelBody.setLayout(new BorderLayout(0, 0));
+
+		panelLeft = new JPanel();
+		panelBody.add(panelLeft, BorderLayout.WEST);
+		panelLeft.setLayout(new BorderLayout(0, 0));
+
+		panelTables = new JPanel();
+		panelTables.setPreferredSize(new Dimension(900, 250));
+		panelLeft.add(panelTables, BorderLayout.CENTER);
+		panelTables.setLayout(null);
+		
+		panelTableNum1 = new JPanel();
+		panelTableNum1.setBorder(UIManager.getBorder("DesktopIcon.border"));
+		panelTableNum1.setBounds(82, 80, 152, 167);
+		panelTables.add(panelTableNum1);
+		panelTableNum1.setLayout(new BorderLayout(0, 0));
+		
+		panelTableHeader1 = new JPanel();
+		panelTableNum1.add(panelTableHeader1, BorderLayout.NORTH);
+		panelTableHeader1.setLayout(new BorderLayout(0, 0));
+		
+		lblTableNum1 = new JLabel("테이블 1");
+		panelTableHeader1.add(lblTableNum1, BorderLayout.NORTH);
+		
+		lblTableTime1 = new JLabel("New label ");
+		lblTableTime1.setHorizontalAlignment(SwingConstants.TRAILING);
+		panelTableHeader1.add(lblTableTime1, BorderLayout.SOUTH);
+		
+		lblNewLabel_2 = new JLabel("New label ");
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.TRAILING);
+		panelTableNum1.add(lblNewLabel_2, BorderLayout.SOUTH);
+		
+		textArea = new JTextArea();
+		panelTableNum1.add(textArea, BorderLayout.CENTER);
+
+		panelRight = new JPanel();
+		panelBody.add(panelRight, BorderLayout.CENTER);
+		panelRight.setLayout(new BorderLayout(0, 0));
+
+		panelOrder = new JPanel();
+		panelRight.add(panelOrder, BorderLayout.CENTER);
+		panelOrder.setLayout(null);
+		
+		panel_2 = new JPanel();
+		panel_2.setBounds(698, 31, 77, 83);
+		panelOrder.add(panel_2);
+		panel_2.setLayout(new BorderLayout(0, 0));
+		
+		lblNewLabel_1 = new JLabel("New label");
+		panel_2.add(lblNewLabel_1, BorderLayout.NORTH);
+		
+		lblNewLabel = new JLabel("테이블 번호");
+		lblNewLabel.setBounds(111, 31, 70, 18);
+		panelOrder.add(lblNewLabel);
+		
+		lblAdults = new JLabel("성인");
+		lblAdults.setBounds(61, 96, 55, 18);
+		panelOrder.add(lblAdults);
+		
+		lblKids = new JLabel("어린이");
+		lblKids.setBounds(61, 126, 55, 18);
+		panelOrder.add(lblKids);
+		
+		tfAdults = new JTextField();
+		tfAdults.setBounds(134, 94, 60, 22);
+		panelOrder.add(tfAdults);
+		tfAdults.setColumns(10);
+		
+		tfKids = new JTextField();
+		tfKids.setColumns(10);
+		tfKids.setBounds(134, 124, 60, 22);
+		panelOrder.add(tfKids);
+		
+		btnAdmission = new JButton("입장");
+		btnAdmission.setBounds(38, 171, 98, 28);
+		panelOrder.add(btnAdmission);
+		
+		lblSoju = new JLabel("소주");
+		lblSoju.setBounds(61, 251, 55, 18);
+		panelOrder.add(lblSoju);
+		
+		lblBeer = new JLabel("맥주");
+		lblBeer.setBounds(61, 281, 55, 18);
+		panelOrder.add(lblBeer);
+		
+		tfSoju = new JTextField();
+		tfSoju.setColumns(10);
+		tfSoju.setBounds(134, 249, 60, 22);
+		panelOrder.add(tfSoju);
+		
+		tfBeer = new JTextField();
+		tfBeer.setColumns(10);
+		tfBeer.setBounds(134, 279, 60, 22);
+		panelOrder.add(tfBeer);
+		
+		lblBeverage = new JLabel("음료수");
+		lblBeverage.setBounds(61, 313, 55, 18);
+		panelOrder.add(lblBeverage);
+		
+		lblSoup = new JLabel("된장찌개");
+		lblSoup.setBounds(61, 343, 55, 18);
+		panelOrder.add(lblSoup);
+		
+		tfBeverage = new JTextField();
+		tfBeverage.setColumns(10);
+		tfBeverage.setBounds(134, 311, 60, 22);
+		panelOrder.add(tfBeverage);
+		
+		tfSoup = new JTextField();
+		tfSoup.setColumns(10);
+		tfSoup.setBounds(134, 341, 60, 22);
+		panelOrder.add(tfSoup);
+		
+		lblDessert = new JLabel("냉면");
+		lblDessert.setBounds(61, 375, 55, 18);
+		panelOrder.add(lblDessert);
+		
+		tfDessert = new JTextField();
+		tfDessert.setColumns(10);
+		tfDessert.setBounds(134, 373, 60, 22);
+		panelOrder.add(tfDessert);
+		
+		btnRenewOrder = new JButton("주문 갱신");
+		btnRenewOrder.setBounds(162, 422, 98, 28);
+		panelOrder.add(btnRenewOrder);
+		
+		btnAdmissionCancel = new JButton("입장 취소");
+		btnAdmissionCancel.setBounds(162, 171, 98, 28);
+		panelOrder.add(btnAdmissionCancel);
+		
+		btnPayment = new JButton("결제하기");
+		btnPayment.setBounds(162, 512, 98, 28);
+		panelOrder.add(btnPayment);
 	}
 
 	private void setComponents() {
+
+		setClock();
+
+	}
+
+	private void setClock() {
+		lblCurrentTime.setVerticalAlignment(SwingConstants.BOTTOM);
+		lblCurrentTime.setHorizontalAlignment(SwingConstants.TRAILING);
+		lblCurrentTime.setFont(new Font("굴림", Font.BOLD, 15));
+		panelHeader.add(lblCurrentTime, BorderLayout.EAST);
+		if (thread == null) {
+			thread = new Thread(this);
+			thread.start();
+		}
 	}
 
 	private void addListener() {
 	}
-
 }
--- src/java0709.java
+++ src/java0709.java
@@ -3,6 +3,7 @@ import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
@@ -16,196 +17,86 @@ import com.jgoodies.forms.layout.ColumnSpec;
 import com.jgoodies.forms.layout.RowSpec;
 import com.jgoodies.forms.layout.FormSpecs;
 import javax.swing.JTextArea;
+import javax.swing.JList;
+import javax.swing.JTable;
+import javax.swing.JCheckBox;
 
 public class java0709 extends JFrame {
-	private JLabel lblNewLabel;
+
 	private JPanel panel;
-	private JButton btnNewButton;
-	private JButton btnNewButton_1;
-	private JButton btnNewButton_2;
 	private JPanel panel_1;
 	private JPanel panel_2;
-	private JLabel lblNewLabel_1;
-	private JLabel lblNewLabel_2;
-	private JTextArea textArea;
-	private JPanel panel_3;
 	private JPanel panel_4;
-	private JLabel lblNewLabel_3;
-	private JLabel lblNewLabel_4;
-	private JTextArea textArea_1;
-	private JLabel lblNewLabel_5;
-	private JLabel lblNewLabel_6;
-	private JTextArea textArea_2;
+	private JPanel panel_3;
 	private JPanel panel_5;
-	private JLabel lblNewLabel_7;
-	private JLabel lblNewLabel_8;
-	private JTextArea textArea_3;
 	private JPanel panel_6;
 	private JPanel panel_7;
 	private JPanel panel_8;
 	private JPanel panel_9;
-	private JLabel lblNewLabel_9;
-	private JLabel lblNewLabel_10;
-	private JLabel lblNewLabel_11;
-	private JLabel lblNewLabel_12;
-	private JLabel lblNewLabel_13;
-	private JLabel lblNewLabel_14;
-	private JLabel lblNewLabel_15;
-	private JLabel lblNewLabel_16;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
+	private JPanel panel_10;
+	private JLabel lblNewLabel;
+	private JLabel lblNewLabel_1;
+	private JPanel panel_11;
+	private JTable table;
+	private JCheckBox chckbxNewCheckBox;
+	private JCheckBox chckbxNewCheckBox_1;
 
-
-
-
-	
-
-	
 	public java0709() {
-		
+
 		Container c = getContentPane();
-		
-		lblNewLabel = new JLabel("New label");
-		getContentPane().add(lblNewLabel, BorderLayout.NORTH);
-		
-		panel = new JPanel();
-		getContentPane().add(panel, BorderLayout.SOUTH);
-		
-		btnNewButton = new JButton("New button");
-		panel.add(btnNewButton);
-		
-		btnNewButton_1 = new JButton("New button");
-		panel.add(btnNewButton_1);
-		
-		btnNewButton_2 = new JButton("New button");
-		panel.add(btnNewButton_2);
-		
+
 		panel_1 = new JPanel();
-		getContentPane().add(panel_1, BorderLayout.CENTER);
-		panel_1.setLayout(null);
-		
+		getContentPane().add(panel_1, BorderLayout.NORTH);
+
+		lblNewLabel = new JLabel("New label");
+		panel_1.add(lblNewLabel);
+
 		panel_2 = new JPanel();
-		panel_2.setBounds(56, 37, 93, 128);
-		panel_1.add(panel_2);
+		getContentPane().add(panel_2, BorderLayout.WEST);
 		panel_2.setLayout(new BorderLayout(0, 0));
-		
-		lblNewLabel_1 = new JLabel("New label");
-		panel_2.add(lblNewLabel_1, BorderLayout.NORTH);
-		
-		lblNewLabel_2 = new JLabel("New label");
-		panel_2.add(lblNewLabel_2, BorderLayout.SOUTH);
-		
-		textArea = new JTextArea();
-		panel_2.add(textArea, BorderLayout.CENTER);
-		
-		panel_3 = new JPanel();
-		panel_3.setBounds(171, 37, 93, 128);
-		panel_1.add(panel_3);
-		panel_3.setLayout(new BorderLayout(0, 0));
-		
-		lblNewLabel_3 = new JLabel("New label");
-		panel_3.add(lblNewLabel_3, BorderLayout.NORTH);
-		
-		lblNewLabel_4 = new JLabel("New label");
-		panel_3.add(lblNewLabel_4, BorderLayout.SOUTH);
-		
-		textArea_1 = new JTextArea();
-		panel_3.add(textArea_1, BorderLayout.CENTER);
-		
-		panel_4 = new JPanel();
-		panel_4.setBounds(288, 37, 93, 128);
-		panel_1.add(panel_4);
-		panel_4.setLayout(new BorderLayout(0, 0));
-		
-		lblNewLabel_5 = new JLabel("New label");
-		panel_4.add(lblNewLabel_5, BorderLayout.NORTH);
-		
-		lblNewLabel_6 = new JLabel("New label");
-		panel_4.add(lblNewLabel_6, BorderLayout.SOUTH);
-		
-		textArea_2 = new JTextArea();
-		panel_4.add(textArea_2, BorderLayout.CENTER);
-		
-		panel_5 = new JPanel();
-		panel_5.setBounds(414, 37, 93, 128);
-		panel_1.add(panel_5);
-		panel_5.setLayout(new BorderLayout(0, 0));
-		
-		lblNewLabel_7 = new JLabel("New label");
-		panel_5.add(lblNewLabel_7, BorderLayout.NORTH);
-		
-		lblNewLabel_8 = new JLabel("New label");
-		panel_5.add(lblNewLabel_8, BorderLayout.SOUTH);
-		
-		textArea_3 = new JTextArea();
-		panel_5.add(textArea_3, BorderLayout.CENTER);
-		
-		panel_6 = new JPanel();
-		panel_6.setBounds(56, 204, 93, 128);
-		panel_1.add(panel_6);
-		panel_6.setLayout(new BorderLayout(0, 0));
-		
-		lblNewLabel_9 = new JLabel("New label");
-		panel_6.add(lblNewLabel_9, BorderLayout.NORTH);
-		
-		lblNewLabel_10 = new JLabel("New label");
-		panel_6.add(lblNewLabel_10, BorderLayout.SOUTH);
-		
-		textField = new JTextField();
-		panel_6.add(textField, BorderLayout.CENTER);
-		textField.setColumns(10);
-		
+
 		panel_7 = new JPanel();
-		panel_7.setBounds(171, 204, 93, 128);
-		panel_1.add(panel_7);
-		panel_7.setLayout(new BorderLayout(0, 0));
-		
-		lblNewLabel_11 = new JLabel("New label");
-		panel_7.add(lblNewLabel_11, BorderLayout.NORTH);
-		
-		lblNewLabel_12 = new JLabel("New label");
-		panel_7.add(lblNewLabel_12, BorderLayout.SOUTH);
-		
-		textField_1 = new JTextField();
-		panel_7.add(textField_1, BorderLayout.CENTER);
-		textField_1.setColumns(10);
-		
+		panel_2.add(panel_7, BorderLayout.NORTH);
+
+		table = new JTable();
+		panel_7.add(table);
+
 		panel_8 = new JPanel();
-		panel_8.setBounds(288, 204, 93, 128);
-		panel_1.add(panel_8);
+		panel_2.add(panel_8, BorderLayout.CENTER);
 		panel_8.setLayout(new BorderLayout(0, 0));
-		
-		lblNewLabel_13 = new JLabel("New label");
-		panel_8.add(lblNewLabel_13, BorderLayout.NORTH);
-		
-		lblNewLabel_14 = new JLabel("New label");
-		panel_8.add(lblNewLabel_14, BorderLayout.SOUTH);
-		
-		textField_2 = new JTextField();
-		panel_8.add(textField_2, BorderLayout.CENTER);
-		textField_2.setColumns(10);
-		
+
 		panel_9 = new JPanel();
-		panel_9.setBounds(414, 204, 93, 128);
-		panel_1.add(panel_9);
-		panel_9.setLayout(new BorderLayout(0, 0));
-		
-		lblNewLabel_15 = new JLabel("New label");
-		panel_9.add(lblNewLabel_15, BorderLayout.NORTH);
-		
-		lblNewLabel_16 = new JLabel("New label");
-		panel_9.add(lblNewLabel_16, BorderLayout.SOUTH);
-		
-		textField_3 = new JTextField();
-		panel_9.add(textField_3, BorderLayout.CENTER);
-		textField_3.setColumns(10);
-		
-		
-		
-		
-		
+		panel_8.add(panel_9, BorderLayout.NORTH);
+
+		lblNewLabel_1 = new JLabel("New label");
+		panel_9.add(lblNewLabel_1);
+
+		panel_10 = new JPanel();
+		panel_8.add(panel_10, BorderLayout.CENTER);
+
+		panel_11 = new JPanel();
+		panel_8.add(panel_11, BorderLayout.WEST);
+
+		panel_4 = new JPanel();
+		getContentPane().add(panel_4, BorderLayout.CENTER);
+		panel_4.setLayout(new BorderLayout(0, 0));
+
+		panel_3 = new JPanel();
+		panel_4.add(panel_3, BorderLayout.NORTH);
+
+		panel_5 = new JPanel();
+		panel_4.add(panel_5, BorderLayout.SOUTH);
+
+		panel_6 = new JPanel();
+		panel_4.add(panel_6, BorderLayout.CENTER);
+
+		chckbxNewCheckBox_1 = new JCheckBox("New check box");
+		panel_6.add(chckbxNewCheckBox_1);
+		chckbxNewCheckBox_1.setBorderPainted(true);
+		chckbxNewCheckBox_1.setPreferredSize(new Dimension(200, 300));
+
+		chckbxNewCheckBox = new JCheckBox("New check box");
+		panel_6.add(chckbxNewCheckBox);
 
 		setSize(600, 500);
 		setVisible(true);
@@ -215,6 +106,3 @@ public class java0709 extends JFrame {
 		new java0709();
 	}
 }
-
-
-		
