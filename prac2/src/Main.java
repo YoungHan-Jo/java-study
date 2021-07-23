340a8ee899376f414e98bef0e94e4196831aa52e
JYH <verin4494@gmail.com>
2021-07-23 14:50:52 +0900

    0723

---
+++ prac2/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    	
+    	List<Person> personList = new ArrayList<>();
+    	
+    }
+}
